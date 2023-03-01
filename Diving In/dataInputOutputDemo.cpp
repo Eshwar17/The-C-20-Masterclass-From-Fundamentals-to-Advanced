afddd6d5529e79b3ccfe35612dd5f34cea38d203
Eshwar17 <65604208+Eshwar17@users.noreply.github.com>
2023-03-01 15:03:37 +0530

    Update dataInputOutputDemo.cpp

--- Diving In/dataInputOutputDemo.cpp
+++ Diving In/dataInputOutputDemo.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*
+    For input we have,
+        cin
+        getline(cin, stringName) -> If you use cin for string it will take the text only before the whitespace(when it reaches whitespace it stops inputting)
+    For output we have,
+        cout
+        cerr -> for displaying error messages
+        clog -> for logging messages
+    */
+   int num;
+   cin >> num;
+   cout << "Number is " << num << endl;
+   string full_name;
+   getline(cin, full_name);
+   cout << "String is " << full_name << endl;
+   cerr << "Error message " << endl;
+   clog << "Logging message " << endl;
+ return 0;
+}
