a3ee47e34652c8410a3d0a204b92228796020778
Eshwar17 <65604208+Eshwar17@users.noreply.github.com>
2023-03-01 14:49:34 +0530

    Update statementsDemo.cpp

--- Diving In/statementsDemo.cpp
+++ Diving In/statementsDemo.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*
+    Statements : 
+    ------------
+        A statement is a basic unit of computation in C++.
+        A C++ program is a collection of statements.
+        A statement ends with semicolon.
+        Executed in order(top to bottom).
 
+    */
+   int first_number {23};
+   int second_number {34};
+   int sum = first_number + second_number;
+   cout << "Sum  : " << sum << endl;
+ return 0;
+}
