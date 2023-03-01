041551df4ad4a76c3eef7c56a13b315e566b972b
Eshwar17 <65604208+Eshwar17@users.noreply.github.com>
2023-03-01 14:18:17 +0530

    Update commentsDemo.cpp

--- Diving In/commentsDemo.cpp
+++ Diving In/commentsDemo.cpp
@@ -1,7 +1,15 @@
-#include<bits/stdc++.h>
+// This brings in the iostream library
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    cout << "Hello World!!" << endl;
- return 0;
+    // Single Line Comment
+    cout << "Single Line comments are written using //" << endl;
+    /*
+        Multiline Comments -> Englobes multiple lines of text
+        !!Another Line!!
+    */
+    cout << "MultiLine comments are written using /* */" << endl;
+    return 0;
+    //Program ends here
 }
