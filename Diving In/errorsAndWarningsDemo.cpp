b5cda461b59a85016f8f02834935cc9c5daf054d
Eshwar17 <65604208+Eshwar17@users.noreply.github.com>
2023-03-01 14:30:29 +0530

    Update errorsAndWarningsDemo.cpp

--- Diving In/errorsAndWarningsDemo.cpp
+++ Diving In/errorsAndWarningsDemo.cpp
@@ -1,1 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Program for practicing errors and warnings in c++ 20" << endl;
+    //Compile Time Errors -> Forgetting semicolons etc..
+    //cout << "Hello World!!" << endl
 
+    //Runtime Errors -> Divide by zero
+    // int value = 23 / 0;
+
+ return 0;
+}
