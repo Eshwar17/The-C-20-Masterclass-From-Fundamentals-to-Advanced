60e82d1eeb5e15cd73e89afa64411e334c8a106d
Eshwar17 <65604208+Eshwar17@users.noreply.github.com>
2023-03-01 17:46:42 +0530

    Update assignment01.cpp

--- Diving In/assignment01.cpp
+++ Diving In/assignment01.cpp
@@ -1,1 +1,17 @@
+/*
+Program prints : Where do you live?
 
+User types : Finland
+
+Program prints : I've heard great things about Finland. I'd like to go sometime.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string place;
+    cout << "Where do you live?" << endl;
+    cin >> place;
+    cout << "I've heard great things about " << place << ". I'd like to go sometime." << endl;
+ return 0;
+}
